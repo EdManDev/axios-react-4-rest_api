f8c084b280765414397fb9295c990df1d9e765d6
EdManDev <ed.manigat@gmail.com>
2020-08-29 19:10:31 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -12,58 +12,64 @@ class App extends Component {
 
 	constructor() {
 		super();
+		// GET -------------------------------------------
+		api.get("/").then((res) => {
+			console.log(res.data);
+			// this.setState({ courses: res.data });
+		});
 	}
-	// // GET------------------------------------
+
+	// // GET -------------------------------------------
 	// getCourses = async () => {
 	// 	let data = await api.get("/").then(({ data }) => data);
 	// 	this.setState({ courses: data });
 	// };
 
-	// GET WITH PARAM------------------------------------
-	getCourses = async () => {
-		let data = await api
-			.get("/", {
-				params: {
-					_limit: 3,
-					_start: 0,
-				},
-			})
-			.then(({ data }) => data);
-		this.setState({ courses: data });
-	};
+	// // GET WITH PARAM ------------------------------------
+	// getCourses = async () => {
+	// 	let data = await api
+	// 		.get("/", {
+	// 			params: {
+	// 				_limit: 3,
+	// 				_start: 0,
+	// 			},
+	// 		})
+	// 		.then(({ data }) => data);
+	// 	this.setState({ courses: data });
+	// };
 
-	// POST ------------------------------------
-	createCourse = async () => {
-		let res = await api.post("/", {
-			title: "test",
-			description: "description test",
-			minimumSkill: "Now test",
-		});
-		console.log(res);
-		this.getCourses();
-	};
+	// // POST ------------------------------------
+	// createCourse = async () => {
+	// 	let res = await api.post("/", {
+	// 		title: "test",
+	// 		description: "description test",
+	// 		minimumSkill: "Now test",
+	// 	});
+	// 	console.log(res);
+	// 	this.getCourses();
+	// };
 
-	// DELETE------------------------------------
-	deleteCourse = async (id) => {
-		let data = await api.delete(`/${id}`);
-		this.getCourses();
-	};
+	// // DELETE ------------------------------------
+	// deleteCourse = async (id) => {
+	// 	let data = await api.delete(`/${id}`);
+	// 	this.getCourses();
+	// };
 
-	// PUT------------------------------------
-	updateCourse = async (id) => {
-		let data = await api.delete(`/${id}`);
-		this.getCourses();
-	};
+	// // PUT ------------------------------------
+	// updateCourse = async (id) => {
+	// 	let data = await api.delete(`/${id}`);
+	// 	this.getCourses();
+	// };
 
 	render() {
 		return (
 			<div>
 				<div>
-					<hr />
+					{/* <hr />
 					{this.state.courses.map((courses) => (
 						<h2 key={courses.id}>{courses.title}</h2>
 					))}
-					<hr />
+					<hr /> */}
 				</div>
 			</div>
 		);
--- src/App_2.jsx
+++ src/App_2.jsx
@@ -2,6 +2,17 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export class App_2 extends Component {
+	state = {
+		name: "",
+	};
+
+	componentDidMount() {
+		axios.get(`http://localhost:5002/api/courses`).then((res) => {
+			console.log(res);
+			this.setState(res);
+		});
+	}
+
 	render() {
 		return (
 			<div>
